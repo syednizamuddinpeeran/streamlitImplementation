5f894db9709e116694c08d33cb6e0a91737d30e6
Syed Nizamuddin Peeran <r.syed.nizamuddin.peeran@gmail.com>
2024-04-28 14:39:04 +0530

    init

---
+++ app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import time
+
+st.title("This is a Demo title")
+st.header("This is a header")
+st.subheader("This is SubHeader")
+st.text("This is a text")
+st.write("This is a st.wrte command. we can pass fucntions that return stuff here")
+def sampleStringreturn():
+    return f"This string comes from function {time.localtime()}" 
+st.write(sampleStringreturn())
+st.write("This *text* is italised by enclosing it in astrics (*)")
+st.write(st.write)
